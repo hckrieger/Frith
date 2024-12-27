73969a1a1fb6981e7e95862bd8b4d1bd3334e2d6
hckrieger <hckrieger77@outlook.com>
2024-12-27 18:25:56 -0500

    remove usings that aren't being used

--- Engine.cs
+++ Engine.cs
@@ -1,16 +1,7 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Frith.Systems;
-using System.Diagnostics;
-using Frith.Events;
 using Frith.Managers;
-using System.Runtime.CompilerServices;
 
 namespace Frith
 {
